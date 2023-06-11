8d02ce80599e6152479e4fa3922820ef4f21553e
Maternus Kuang <103388505+mk17041@users.noreply.github.com>
2023-06-11 23:31:26 +1200

    bcrypt
    made it so the login automatically  checks the hashed password instead of the non hashed password

--- Scholarship/app.py
+++ Scholarship/app.py
@@ -1,11 +1,11 @@
 from pickle import TRUE
-#import MySQLdb
-from flask import Flask, render_template, request, redirect, jsonify, session, flash, logging
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, session
 import mysql.connector
 import bcrypt
+
+app = Flask(__name__)
 app.secret_key = 'thisisoursecretkey'
-    
+
 # Function to hash the password
 def hash_password(password):
     hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
@@ -13,7 +13,7 @@ def hash_password(password):
 
 # Function to insert a user with hashed password
 def insert_user(username, password):
-# Connect to MySQL
+    # Connect to MySQL
     db = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -21,61 +21,50 @@ def insert_user(username, password):
         database="userdata"
     )
 
-# Create a cursor object to interact with the database
+    # Create a cursor object to interact with the database
     mycursor = db.cursor()
 
- # Hash the password
+    # Hash the password
     hashed_password = hash_password(password)
 
-# Insert the user into the database
+    # Insert the user into the database
     query = "INSERT INTO users (username, password_hash) VALUES (%s, %s)"
     values = (username, hashed_password)
     mycursor.execute(query, values)
 
-
- # Commit the changes to database
+    # Commit the changes to database
     db.commit()
 
-# Close the cursor and database connection
+    # Close the cursor and database connection
     mycursor.close()
     db.close()
 
 
-
-app.debug = True
-
-#database setup
-db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passwd="root1234",
-        database="userdata"
-    )
-
-
-@app.route("/login1", methods = ["POST","GET"])
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    #if user submits form 
     if request.method == "POST":
-        #creating varibles from form data from html
         username = request.form["username"]
         password = request.form["password"]
-        #cur = open data base
+        
+        db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="root1234",
+            database="userdata"
+        )
         mycursor = db.cursor(buffered=True, dictionary=True)
-        #searches in logintable where username/password is = to what user inputted
-        mycursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
-        login = mycursor.fetchone()
-        #if true transfers to menu
-        if login: 
-                session["loggedin"] = TRUE
-                session["username"] = login["username"]
-                return redirect("/")
-        #else outputs 'wrong username or password'
+        mycursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+        user = mycursor.fetchone()
+        
+        if user and bcrypt.checkpw(password.encode("utf-8"), user["password_hash"].encode("utf-8")):
+            session["loggedin"] = TRUE
+            session["username"] = user["username"]
+            return redirect("/")
         else:
-            error = 'wrong username or password'
-            return render_template("login.html", error = error)
-    else: 
+            error = 'Wrong username or password'
+            return render_template("login.html", error=error)
+    else:
         return render_template("login.html")
-    
+
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
