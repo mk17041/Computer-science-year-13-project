96e73860d690a115f9c507068993b82be60409dd
Maternus Kuang <103388505+mk17041@users.noreply.github.com>
2023-06-14 17:48:55 +1200

    Sign up
    completed the flask/python bit just have to do some minor html

--- Scholarship/app.py
+++ Scholarship/app.py
@@ -2,6 +2,7 @@ from pickle import TRUE
 from flask import Flask, render_template, request, redirect, session
 import mysql.connector
 import bcrypt
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.secret_key = 'thisisoursecretkey'
@@ -39,32 +40,60 @@ def insert_user(username, password):
     mycursor.close()
     db.close()
 
-
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
-        
-        db = mysql.connector.connect(
+    # Database setup
+db = mysql.connector.connect(
             host="localhost",
             user="root",
             passwd="root1234",
             database="userdata"
         )
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    # If user submits form 
+    if request.method == "POST":
+
+        # Creating varibles from form data from html
+        username = request.form["username"]
+        password = request.form["password"]
+
+        # mycursor = open data base
         mycursor = db.cursor(buffered=True, dictionary=True)
+
+        # Searches in users where username/password is = to what user inputted
         mycursor.execute("SELECT * FROM users WHERE username = %s", (username,))
         user = mycursor.fetchone()
-        
+
+        # If true transfers to menu
         if user and bcrypt.checkpw(password.encode("utf-8"), user["password_hash"].encode("utf-8")):
             session["loggedin"] = TRUE
             session["username"] = user["username"]
             return redirect("/")
+        
+        # Else outputs 'wrong username or password'
         else:
             error = 'Wrong username or password'
             return render_template("login.html", error=error)
     else:
         return render_template("login.html")
+    
 
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        # Insert the user into the database with hashed password
+        mycursor = db.cursor()
+        query = "INSERT INTO users (username, password_hash) VALUES (%s, %s)"
+        values = (username, hash_password(password))
+        mycursor.execute(query, values)
+        db.commit()
+
+        return redirect("/login")
+    else:
+        return render_template("signup.html")
+    
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
