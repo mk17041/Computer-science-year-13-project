bc138feb9b2d03ad53fc9a72833c954b957961dc
Maternus Kuang <103388505+mk17041@users.noreply.github.com>
2023-06-23 19:53:46 +1200

    Added circle button + changes to the calorie bar

--- Scholarship/static/style.js
+++ Scholarship/static/style.js
@@ -2,19 +2,28 @@
 
 // Function to enable auto scrolling of the carousel
 function enableAutoScroll() {
-    setInterval(function() {
-      const carousel = document.querySelector('#carouselExampleIndicators');
-      const currentIndex = Number(carousel.querySelector('.carousel-indicators .active').getAttribute('data-bs-slide-to'));
-      const totalSlides = carousel.querySelectorAll('.carousel-indicators button').length;
-  
-      // Calculate the index of the next slide
-      const nextIndex = (currentIndex + 1) % totalSlides;
-  
-      // Go to the next slide
-      carousel.querySelector(`.carousel-indicators button[data-bs-slide-to="${nextIndex}"]`).click();
-    }, 5000); // Adjust the time interval (in milliseconds) as per your requirement
-  }
-  
-  // Call the function to enable auto scrolling
-  enableAutoScroll();
-  
+  setInterval(function () {
+    const carousel = document.querySelector("#carouselExampleIndicators");
+    const currentIndex = Number(
+      carousel
+        .querySelector(".carousel-indicators .active")
+        .getAttribute("data-bs-slide-to")
+    );
+    const totalSlides = carousel.querySelectorAll(
+      ".carousel-indicators button"
+    ).length;
+
+    // Calculate the index of the next slide
+    const nextIndex = (currentIndex + 1) % totalSlides;
+
+    // Go to the next slide
+    carousel
+      .querySelector(
+        `.carousel-indicators button[data-bs-slide-to="${nextIndex}"]`
+      )
+      .click();
+  }, 5000); // Adjust the time interval (in milliseconds) as per your requirement
+}
+
+// Call the function to enable auto scrolling
+enableAutoScroll();
